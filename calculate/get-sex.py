502f47c72770eae9e9c121608c81ff19162b8159
Gerrit Botha <gerrit.botha@uct.ac.za>
2025-03-24 13:31:43 +0200

    Sex are calcuated directly on VCF

--- calculate/get-sex.py
+++ calculate/get-sex.py
@@ -27,9 +27,15 @@ def main():
     for sample in sample_list:
         sample_vcf_file = os.path.join(tmp_dir, f"{sample}.vcf.gz")
         extract_sample_from_vcf(vcf_file, sample, sample_vcf_file)
-        get_bcftools_stats(sample_vcf_file, f"{sample_vcf_file}.stats")
-        hetero, homo, ratio = calculate_hetero_homo_ratio(f"{sample_vcf_file}.stats")
+        hetero, homo, ratio = calculate_hetero_homo_ratio(sample_vcf_file)
         print(f"Sample: {sample}, Heterozygous: {hetero}, Homozygous: {homo}, Ratio: {ratio}")
+        if ratio < 0.01:
+            sex = "male"
+        elif 0.4 < ratio < 0.6:
+            sex = "female"
+        else:
+            sex = "unknown"
+        print(f"Sample: {sample}, Sex: {sex}")
 
 def get_samples_from_vcf(vcf_file):
     """
@@ -64,28 +70,14 @@ def extract_sample_from_vcf(vcf_file, sample_name, output_file):
     ]
     subprocess.run(command, check=True)
 
-def get_bcftools_stats(vcf_file, output_file):
-    """
-    Get bcftools stats for a VCF file.
-
-    :param vcf_file: Path to the input VCF file.
-    :param output_file: Path to the output stats file.
-    """
-    command = [
-        "bcftools", "stats",
-        "--output", output_file,
-        vcf_file
-    ]
-    subprocess.run(command, check=True)
-
-def calculate_hetero_homo_ratio(vcf_file_stats):
+def calculate_hetero_homo_ratio(vcf_file):
     """
     Calculate the heterozygous and homozygous counts and their ratio from a VCF file.
 
     :param vcf_file: Path to the input VCF file.
     :return: Tuple containing heterozygous count, homozygous count, and their ratio.
     """
-    command = f"bcftools query -f '%CHROM\\t%POS\\t[%GT]\\n' {vcf_file_stats} | awk -F '\\t' '{{split($3, genotypes, \"/\"); if (genotypes[1] != genotypes[2]) hetero++;else homo++;}}END{{print hetero, homo, hetero / homo}}'"
+    command = f"bcftools query -f '%CHROM\\t%POS\\t[%GT]\\n' {vcf_file} | awk -F '\\t' '{{split($3, genotypes, \"/\"); if (genotypes[1] != genotypes[2]) hetero++;else homo++;}}END{{print hetero, homo, hetero / homo}}'"
     result = subprocess.run(command, shell=True, capture_output=True, text=True, check=True)
     hetero, homo, ratio = result.stdout.strip().split()
     return int(hetero), int(homo), float(ratio)
